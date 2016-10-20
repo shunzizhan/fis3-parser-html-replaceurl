15f1d9adc2b5243e21987f601e8599cde20def41
shunzizhan <shunzizhan@163.com>
2016-10-20 09:17:59 +0800

    v0.2.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 module.exports = function (content, file, opt) {
   // 只对 html 类文件进行处理
-  if (!file.isHtmlLike){
-    return content;
-  }
+  // if (!file.isHtmlLike){
+  //   return content;
+  // }
   var newWords = opt.newWords,
       ignoreCommentWords = opt.ignoreWords || [],
       ignoreWordsReg = new RegExp(ignoreCommentWords.join('|'),'g');
@@ -11,19 +11,23 @@ module.exports = function (content, file, opt) {
     var Reg = new RegExp(ele,'g');
     content = content.replace(Reg,newWords[ele]);
   }
-  // 删除注释
-  if(opt.removeComments){
-    content = content.replace(/(<!--)(.{0,})(-->)/g,function(match,g1,g2,g3,index,origin){
-      if(g2.search(ignoreWordsReg)>-1){
-        return g1+g2+g3;
-      }else{
-        return "";
-      }
-    });
+  
+  if(file.isHtmlLike){
+    // 删除注释
+    if(opt.removeComments){
+      content = content.replace(/(<!--)(.{0,})(-->)/g,function(match,g1,g2,g3,index,origin){
+        if(g2.search(ignoreWordsReg)>-1){
+          return g1+g2+g3;
+        }else{
+          return "";
+        }
+      });
+    }
+    // 压缩文件
+    if(opt.minifier){
+      content = content.replace(/>\s+</gm,"><");
+    }
   }
-  // 压缩文件
-  if(opt.minifier){
-    content = content.replace(/>\s+</gm,"><");
-  }
+  
   return content;
 };
